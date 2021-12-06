eba8efc706efa66f27c972222e336183ec31acd0
Kryonautine <vkrishnachaitanyag@gmail.com>
2021-12-06 20:25:27 +0530

    A program to print multiplication tables.

--- tables.c
+++ tables.c
@@ -2,5 +2,15 @@
 
 void main()
 {
+	int ele, product, i;
+	i = 1;
+	printf("Enter the number : ");
+	scanf("%d",&ele);
 
+	loop:
+	product = ele * i;
+	printf("%d X %d = %d\n", ele, i, product);
+	i += 1;
+	if (i <= 10)
+	goto loop;
 }
