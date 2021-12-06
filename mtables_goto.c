f09ca0d38673f5ec30771db044b4ba47f949672e
Kryonautine <vkrishnachaitanyag@gmail.com>
2021-12-06 20:30:46 +0530

    Added Comments for unclear statements.

--- mtables_goto.c
+++ mtables_goto.c
@@ -2,15 +2,22 @@
 
 void main()
 {
+
 	int ele, product, i;
 	i = 1;
 	printf("Enter the number : ");
 	scanf("%d",&ele);
 
 	loop:
+
+	// loop is a label to execute goto statement.
+
 	product = ele * i;
 	printf("%d X %d = %d\n", ele, i, product);
 	i += 1;
+
 	if (i <= 10)
+	
 	goto loop;
+
 }
