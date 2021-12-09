bbf3edbe1ed971aaabfb0ccf549db0be2ffcbfa7
Kryonautine <vkrishnachaitanyag@gmail.com>
2021-12-09 13:15:41 +0530

    Added comments to programs.

--- pythag_trip.c
+++ pythag_trip.c
@@ -1,45 +1,66 @@
 #include <stdio.h>
 
+/*
+ * tripfind is a function that finds the first N pythagorean triplets.
+ * depending on the value of (num3 * num3 - num2 * num2),
+ * it either increases num1,num2 or num3 in order to bring the value
+ * closer to (num1 * num1).
+*/
 void tripfind(int *N)
 {
-	int a=3, b=4, c=5, nCheck =0;
+	// (3,4,5) is the smallest pythagorean triplet. nCheck is the number of pythagorean triplets printed.
+	int num1=3, num2=4, num3=5, nCheck =0;
 	while (nCheck < *N)
 	{
-		if (c * c - b * b >= a * a)
+		if (num3 * num3 - num2 * num2 >= num1 * num1)
 		{
-			if (c * c - b * b == a * a)
+			if (num3 * num3 - num2 * num2 == num1 * num1)
 			{
-				printf("(%d,%d,%d)\n", a, b, c);
+				printf("(%d,%d,%d)\n", num1, num2, num3);
 				nCheck++;
-				if (c - b == 1)
+
+				/* 
+				 * if (num3-num2)= 1, then we can assume that there are no more pythagorean triplets
+				 * including num1 that are greater than (current) num2,num3 because then for all values
+				 *  greater , (num3*num3 - num2*num2) > num1*num1.
+				*/
+
+				if (num3 - num2 == 1)
 				{
-					a++;
-					b = a + 1;
-					c = a + 2;
+					num1+=1;
+					num2 = num1 + 1;
+					num3 = num1 + 2;
 				}
 				else
 				{
-					c +=1;
+					num3 +=1;
 				}
 			}
 			else
 			{
-				if (c - b == 1)
+
+				/* 
+				 * if (num3-num2)= 1, then we can assume that there are no more pythagorean triplets
+				 * including num1 that are greater than (current) num2,num3 because then for all values
+				 * (num3*num3 - num2*num2) > num1*num1.
+				*/
+
+				if (num3 - num2 == 1)
 				{
-					a +=1;
-					b = a + 1;
-					c = a + 2;
+					num1 +=1;
+					num2 = num1 + 1;
+					num3 = num1 + 2;
 				}
 				else
 				{
-					b+=1;
+					num2+=1;
 				}
 			}
 
 		}
 		else
 		{
-			c+=1;
+			num3+=1;
 		}
 	}
 }
